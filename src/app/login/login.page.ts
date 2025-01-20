7b35ceab958f07264d6e9206bf8061189ac04f9e
JOSE MANUEL LARA VILLALOBOS <20233l001152@utcv.edu.mx>
2025-01-20 13:12:39 -0600

    feat: conectar input

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
+
+  constructor(
+    private navController: NavController,
+    private toastController: ToastController,
+
+  ) {}
 
   ngOnInit() {
   }
+  login(){
+    if(this.email === 'jose@gmail.com' && this.password === 'admin123'){
+    this.navController.navigateRoot('/home');
+  }
+  else{
+    this.presentToast();
+  }
+  }
 
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'DATOS INCORRECTOS!',
+      duration: 3000,
+      position: 'bottom',
+    });
+
+    await toast.present();
+  }
 }
